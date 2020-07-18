dc2a266b3e14bb483a7219db24d01f49f54b539a
superDongE <dlagoehd13@gmail.com>
2020-07-19 06:38:20 +0900

    Updates

---
+++ precache-manifest.60b81e9235a40ec6ff50d0a6a3c20960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8132a406dd4729a1dfaa211edd517fa9",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "b2afedd87df8ab06905a",
+    "url": "/ReactTodoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e151ba098a37f593dc25",
+    "url": "/ReactTodoList/static/js/2.07ef9514.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactTodoList/static/js/2.07ef9514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2afedd87df8ab06905a",
+    "url": "/ReactTodoList/static/js/main.ae1f17c5.chunk.js"
+  },
+  {
+    "revision": "71c422f3d1a379558a79",
+    "url": "/ReactTodoList/static/js/runtime-main.384c32ff.js"
+  }
+]);
